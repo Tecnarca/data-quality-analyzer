c74985dec1097da942147761aae6347d61e96bb5
Tecnarca <mail.tecnarca@gmail.com>
2019-07-15 05:42:48 +0200

    Refactored part of the code and implemented half of the style

--- webapp.py
+++ webapp.py
@@ -38,6 +38,9 @@ for file_ in csvs:
 	s_df = spark.read.csv(file_,header = True,schema=dfStruct)
 	df.append(s_df)
 	#p.append(spark_df_profiling.ProfileReport(s_df).rendered_html())
+#developing profiling, remove in release
+#p.append(spark_df_profiling.ProfileReport(df[0]).rendered_html())
+#p.append(spark_df_profiling.ProfileReport(df[1]).rendered_html())
 print("[Start] Loaded and profiled Spark data frames")
 
 print("[Start] Starting Flask...")
@@ -84,24 +87,20 @@ def query():
 		c=[]
 		suggested=[]
 		for idx, dfc in enumerate(df):
-			if request.form.get('d'+str(idx)) is not None:
+			if request.form.get('d') == str(idx):
 				c.append(idx)
 		if len(c)!=1:
 			return "ERROR: You must select 1 day to query"
 		df[c[0]].createOrReplaceTempView("PEOPLE")		
-		if request.form.get('form2') is not None:
-			if request.form.get('query') is not None:
-				sqlDF = spark.sql(request.form.get('query'))
-				pdf = sqlDF.toPandas().head(20).to_html(classes='table')
-				suggested = qualityAttrs(request.form.get('query'))
-				if len(suggested) > 1:
-					print(suggested)
-					return render_template('query.html', dfs=df, attrs=df[0].columns, pdf=pdf, query=request.form.get('query'), day=c[0], suggested=suggested)
-				else:
-					return render_template('query.html', dfs=df, attrs=df[0].columns, pdf=pdf, query=request.form.get('query'), day=c[0])
+		if request.form.get('query') is not "":
+			sqlDF = spark.sql(request.form.get('query'))
+			pdf = sqlDF.toPandas().head(20).to_html(classes='table')
+			suggested = qualityAttrs(request.form.get('query'))
+			if len(suggested) > 1:
+				return render_template('query.html', dfs=df, attrs=df[0].columns, pdf=pdf, query=request.form.get('query'), day=c[0], suggested=suggested)
 			else:
-				return "ERROR: Query cannot be empty"
-		elif request.form.get('form1') is not None:
+				return render_template('query.html', dfs=df, attrs=df[0].columns, pdf=pdf, query=request.form.get('query'), day=c[0])
+		else:
 			query = "SELECT "
 			for attr in df[0].columns:
 				if  request.form.get('s'+attr) is not None:
@@ -133,12 +132,8 @@ def query():
 					query = query[:-14]
 				else:
 					query = query[:-1]+')'
-					if request.form.get('min') is not None:
-						query = query + '<'
-					elif request.form.get('max') is not None:
-						query = query + '>'
-					elif request.form.get('eq') is not None:
-						query = query + '='
+					if request.form.get('op') is not None:
+						query = query + request.form.get('op')
 					else:
 						print("Error on query: " + query)
 						return "ERROR building the query: max min or eq must be non null when attrs to count are selected. <br/> Query:" + query
@@ -149,13 +144,11 @@ def query():
 						return "ERROR building the query: " + query
 			print(query)
 			sqlDF = spark.sql(query)
-			pdf = sqlDF.toPandas().head(20).to_html(classes='table')
+			pdf = sqlDF.toPandas().head(20).to_html(classes='table-responsive')
 			if len(suggested) > 1:
 				return render_template('query.html', dfs=df, attrs=df[0].columns, pdf=pdf, query=query, day=c[0], suggested=suggested)
 			else:
 				return render_template('query.html', dfs=df, attrs=df[0].columns, pdf=pdf, query=query, day=c[0])
-		else:
-			return "Valid form not received"
 	else:
 		return render_template('query.html', dfs=df, attrs=df[0].columns)
 
